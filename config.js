5595d607b01603f72443dc2dd327b85e6302a566
AndreevAV <AndreevAndreiV@gmail.com>
2021-01-19 17:48:41 +0300

    add Socket.IO, Express step #2

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ const root = path.join.bind(this, __dirname)
 dotenv.config({path: root('.env')})
 
 module.exports = {
+  HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || 3000
 }
--- index.js
+++ index.js
@@ -1,50 +1,15 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
 const globalConfig = require('./config')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
-    const ext = path.extname(filePath)
-    let contentType = 'text/html'
+app.use(express.static(__dirname +'/public'));
 
-    if (!ext) {
-        filePath += '.html'
-    }
+io.on('connection', (socket) => {
+    console.log('a user connected, '+ socket);
+  });
 
-    switch (ext) {
-        case '.css':
-            contentType = 'text/css'
-            break
-        case '.js':
-            contentType = 'text/javascript'
-            break
-        case '.test':
-            contentType = 'text/plain'
-            break
-        default:
-            contentType = 'text/html'
-            // break
-    }
-
-    console.log(req.url)
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            throw err
-        } else {
-            res.writeHead(200, {'Content-Type': contentType})
-            res.end(content)
-        }
-    })
-})
-
-// const express = require('express')
-// const app = express()
-
-// app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
-
-const chalk = require('chalk')
-
-server.listen(globalConfig.PORT, () => {
-    console.log('Server has been started on ' + chalk.blue(globalConfig.PORT) + '...')
-})
+http.listen(globalConfig.PORT, globalConfig.HOST, () => {
+  console.log(`listening on '${globalConfig.PORT}:${globalConfig.HOST}`);
+});
